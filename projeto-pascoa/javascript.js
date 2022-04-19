0d5549c0d47e74c8d80ef4be0cf294a02f309fd1
Beatriz Ulian <beatrizh.ulian@gmail.com>
2022-04-19 09:49:17 -0300

    .

--- projeto-pascoa/javascript.js
+++ projeto-pascoa/javascript.js
@@ -12,20 +12,31 @@
     }
 
     function replicar(){
-
+let i = 0;
         for (const p of lista) {
+            let id = i;
             let produto = document.querySelector('.ovos').cloneNode(true);
             produto.querySelector('.titulosabores').innerHTML = p.nome;
             produto.querySelector('.descricao').innerHTML = p.descricao;
             produto.querySelector('.valor').innerHTML = p.valor;
-            produto.querySelector('#quantidade').innerHTML = p.quantidade;
-            // produto.querySelector('.img').innerHTML = p.img;
+            produto.querySelector('.quantidade').innerHTML = p.quantidade;
+            produto.querySelector('img').innerHTML = p.img;
+         
+            produto.querySelector(".menos").addEventListener("click", function () { alterarQt(id, -1) });
+            produto.querySelector(".mais").addEventListener("click", function () { alterarQt(id, 1) });
+
             document.querySelector('.lista').append(produto);
-    
+            i++;
         }
         document.querySelector('.ovos').remove();
     }
 
+    function alterarQt(id, quantidade) {
+        let p = lista[id];
+        p.quantidade += quantidade;
+        if(p.quantidade < 0 ) p.quantidade = 0;
+        document.getElementsByClassName("quantidade")[id].innerHTML = p.quantidade;
+    }
     buscarListaProduto();
 
 
